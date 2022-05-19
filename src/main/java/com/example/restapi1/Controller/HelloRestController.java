b0d335d3c0a79f20f2bf4cbca7495b47de5938d4
Akshayku005 <99657121+Akshayku005@users.noreply.github.com>
2022-05-19 18:12:54 +0530

    Added: created rest controller and showed message

--- src/main/java/com/example/restapi1/Controller/HelloRestController.java
+++ src/main/java/com/example/restapi1/Controller/HelloRestController.java
@@ -12,24 +12,5 @@ public class HelloRestController {
     public String message() {
         return "Hello From BridgeLabz";
     }
-
-    @GetMapping("/mr")
-    public String sayHello(@RequestParam String name) {
-        return "Hello "+name+" From BridgeLabz";
-    }
-
-    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-    public String sayHelloParam1 (@RequestParam(value="name")String name) {
-        return "Hello " + name + "!";
-    }
-
-    @PostMapping("/post")
-    public String userData(@RequestBody User user) {
-        return "Hello "+user.getfName()+" "+user.getlName()+" from BridgeLabz";
-    }
-    @PutMapping("/put/{fName}")
-    public String sayHello(@PathVariable String fName,@RequestParam String lName){
-        return "Hello "+fName+" "+lName+ " from BridgeLabz";
-    }
     }
 
